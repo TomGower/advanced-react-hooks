cd3de85c3e4a17e1711a98b2b8b39f17ac147a41
TomGower <tom.gower@gmail.com>
2021-01-20 17:32:58 -0600

    Clarified text in comment

--- src/exercise/01ec2.js
+++ src/exercise/01ec2.js
@@ -8,7 +8,7 @@ const countReducer = (previousCount, change) => {
 }
 // KCD version
 // const countReducer = (state, action) => ({...state, ...action})
-// key is to spread the change (or action, in Redux) to destructure the changed property names
+// key is to spread the change (or action, in Redux parlance) to destructure the changed property name(s)
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
