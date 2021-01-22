c5a53feceb1558b45c1a1f08eca0d8c1f97800be
TomGower <tom.gower@gmail.com>
2021-01-22 14:12:28 -0600

    Solved exercise 06

--- src/exercise/06original.js
+++ src/exercise/06original.js
@@ -5,7 +5,8 @@ import * as React from 'react'
 
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  React.useDebugValue(`\`${query}\` => ${state}`)
+  // 🐨 call React.useDebugValue here.
+  // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
 
   React.useEffect(() => {
     let mounted = true
